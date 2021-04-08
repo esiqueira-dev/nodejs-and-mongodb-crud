5adde9218e5dd9ab79127591354b812c86653121
esiqueira <esiqueira.dev@gmail.com>
2021-04-07 22:46:34 -0300

    Innitial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import routes from './routes';
+
+mongoose.connect(process.env.MONGO_DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
+const app = express();
+
+app.use(express.json());
+app.use(routes)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
